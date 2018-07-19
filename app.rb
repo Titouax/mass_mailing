49eb5f9daaebe9b207c600078d9fba5ca0b7b5d7
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-19 17:41:00 +0200

    fourth commit Pascal

--- app.rb
+++ app.rb
@@ -5,3 +5,4 @@ $:.unshift File.expand_path("./../lib", __dir__)
 require_relative "./lib/views/index.rb"
 require_relative "./lib/views/done.rb"
 require_relative './lib/app/townhalls_mailer'
+require_relative "./lib/app/townhalls_scrapper.rb"
