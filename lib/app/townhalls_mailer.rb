6d45ddecdae60c759f979664dd8ccbcbf619e5f2
Titouax <titouan.dessus@gmail.com>
2018-07-19 20:13:40 +0200

    Mailer fonctionnel & chemin relatif pour la DB du scrapper

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -1,25 +1,45 @@
 require 'gmail'
 require 'dotenv'
-
+require 'csv'
 
 class Mailer
+  attr_accessor :array
   def initialize
-    gmail = Gmail.connect(USERNAME_GMAIL, PASSWORD_GMAIL)
+    @array = []
+    #A completer avant de lancer le code 
+    gmail = Gmail.connect("email", "password")
+    send(parse)
   end
 
-  def send_mail
-    gmail.deliver do
-      to "email@example.com"
-      subject "Having fun in Puerto Rico!"
-      text_part do
-        body "Text of plaintext message."
-      end
-      html_part do
-        content_type 'text/html; charset=UTF-8'
-        body "<p>Text of <em>html</em> message.</p>"
-      end
-      add_file "/path/to/some_image.jpg"
-    end
+  def parse
+    CSV.foreach('../../db/townhalls.csv').map { |col|
+       @array << col[1]
+     }
+     p @array
+     return @array
   end
-  
+
+  def send(array)
+    #array2 = ["team.thp.rescue@gmail.com"]
+    gmail = Gmail.connect("team.thp.rescue@gmail.com"," Thp2018rescue")
+    array.each do |x|
+    email = gmail.compose do
+    to "#{x}"
+    subject "thp"
+       body "Bonjour,
+        Je m'appelle [PRÉNOM], je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection,
+        sans restriction géographique. La pédagogie de ntore école est celle du peer-learning,
+        où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.
+        Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie
+        veut changer le monde avec nous ?
+        Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
+      end
+        gmail.deliver(email)
+      end
+
+
+    gmail.logout
+  end
 end
+
+Mailer.new
