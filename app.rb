ee549059bb281851f7da317c5da84ac29acac4a3
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-19 21:56:54 +0200

    Eighth commit Pascal

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require "bundler"
 Bundler.require
 $:.unshift File.expand_path("./../lib", __dir__)
-
+$:.unshift File.expand_path('./../db', __dir__)
+$:.unshift File.expand_path('lib/views'__dir__)
 Dotenv.load
 require_relative "./lib/views/index.rb"
 require_relative "./lib/views/done.rb"
