f9bf5e7235b34f411deba70133cb4697bf921281
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-19 20:30:58 +0200

    Fifth commit Pascal

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ require_relative "./lib/views/index.rb"
 require_relative "./lib/views/done.rb"
 require_relative './lib/app/townhalls_mailer'
 require_relative "./lib/app/townhalls_scrapper.rb"
+
+Mailer.new
+Scrapper.new
