4c0f3464c2d97676247793045eb080e0328361ea
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-19 21:38:52 +0200

    Seventh commit Pascal

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "bundler"
 Bundler.require
 $:.unshift File.expand_path("./../lib", __dir__)
 
+Dotenv.load
 require_relative "./lib/views/index.rb"
 require_relative "./lib/views/done.rb"
 require_relative './lib/app/townhalls_mailer'
