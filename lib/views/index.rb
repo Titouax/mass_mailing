7c97effeed46f26a783a49de66198eaf1749aa29
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-19 20:55:02 +0200

    Debut d'index

---
+++ lib/views/index.rb
@@ -0,0 +1,17 @@
+class Menu 
+
+
+	def choice
+		puts "Entrez 1 pour recuperer les emails de trois departements, 2 pour envoyer des mails"
+		print ">"
+		choice = gets.chomp.to_i
+
+		if choice == 1 
+			Mailer.new
+		if choice == 2
+			Scrapper.new
+		else
+			puts "Mauvais choix"
+		end
+	end
+
